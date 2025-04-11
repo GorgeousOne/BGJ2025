f06b4571487022134002d7135c0db92d8d2a5eed
Hannes <35993346+Hanesto@users.noreply.github.com>
2025-04-11 21:28:48 +0200

    Added Ingredient Scritable object

---
+++ Assets/Scripts/Ingredients/Ingredient.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient", menuName = "Ingredient", order = 0)]
+public class Ingredient : ScriptableObject 
+{
+    public string ingredientName;
+    public Sprite ingredientImage;
+    
+}
