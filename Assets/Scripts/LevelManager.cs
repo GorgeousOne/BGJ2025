abeec11a1f7a00b096073b56d086319b0c242eb1
Hannes <35993346+Hanesto@users.noreply.github.com>
2025-04-12 17:31:38 +0200

    levelselect

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour
 {
     public Potion currentPotion;
     public Volume globalVolume;
+    public GameObject endscreen;
     ColorAdjustments colorAdjustments;
     public Slot[] slots;
     public static LevelManager instance;
@@ -16,7 +17,7 @@ public class LevelManager : MonoBehaviour
         instance = this;
     }
 
-    void Start()
+    public void StartLevel()
     {
         for(int i = 0; i < currentPotion.ingredients.Length; i++){
             slots[i].SetItem(currentPotion.ingredients[i].ingredient);
@@ -36,9 +37,10 @@ public class LevelManager : MonoBehaviour
 
     IEnumerator Flashbang()
     {
-        LeanTween.value(gameObject, 0f, 6f, 0.1f).setOnUpdate((float val) => { colorAdjustments.postExposure.Override(val); } );
+        LeanTween.value(gameObject, 0f, 9f, 0.1f).setOnUpdate((float val) => { colorAdjustments.postExposure.Override(val); } );
         yield return new WaitForSeconds(0.5f);
-        LeanTween.value(gameObject, 6f, 0f, 8f).setOnUpdate((float val) => { colorAdjustments.postExposure.Override(val); } );
+        endscreen.SetActive(true);
+        LeanTween.value(gameObject, 6f, 0f, 6f).setOnUpdate((float val) => { colorAdjustments.postExposure.Override(val); } );
     }
 }
 
--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelSelection : MonoBehaviour
 {
+    public GameObject levelSelectionHolder;
+    public Transform ingredientsHolder;
+    public GameObject ingredientPrefab;
     public Button startLevelButton, lastLevelButton, nextLevelButton;
     public Potion[] levels;
     public TMPro.TMP_Text potionName, potionDescription;
     public LevelManager levelManager;
+    List<GameObject> currentIngredients = new();
     int currentLevel;
 
     void Start()
@@ -14,6 +19,8 @@ public class LevelSelection : MonoBehaviour
         startLevelButton.onClick.AddListener(StartLevel);
         lastLevelButton.onClick.AddListener(LastLevel);
         nextLevelButton.onClick.AddListener(NextLevel);
+
+        SetLevel(0);
     }
 
     void LastLevel()
@@ -33,11 +40,20 @@ public class LevelSelection : MonoBehaviour
     void StartLevel()
     {
         levelManager.currentPotion = levels[currentLevel];
+        levelSelectionHolder.SetActive(false);
+        levelManager.StartLevel();
     }
 
     void SetLevel(int index)
     {
         potionName.text = levels[index].potionName;
         potionDescription.text = levels[index].description;
+        foreach (GameObject ingredient in currentIngredients){ Destroy(ingredient); }
+        currentIngredients.Clear();
+        foreach (LevelIngredient ingredient in levels[currentLevel].ingredients){
+            GameObject current = Instantiate(ingredientPrefab, ingredientsHolder);
+            current.GetComponent<MenuIngredient>().image.sprite = ingredient.ingredient.ingredientImage;
+            currentIngredients.Add(current);
+        }
     }
 }
